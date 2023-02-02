d766c35b1b995b7362f5f797b259817131fa5cf2
Sachio90 <124223479+Sachio90@users.noreply.github.com>
2023-02-01 23:20:26 -0300

    Add files via upload

---
+++ func.js
@@ -0,0 +1,7 @@
+document.getElementById('prueba').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'blue';
+});
+
+document.getElementById('boton_rest').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'white';
+});
